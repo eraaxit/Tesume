7594f2f9f5b44133eddbc3fa40f6fc9dc4bc9d6a
mahi-14 <mahi1206@chitkara.edu.in>
2021-12-07 22:39:26 +0530

    changes

--- src/Components/Pages/BrowseTemplates/BrowseTemplates.jsx
+++ src/Components/Pages/BrowseTemplates/BrowseTemplates.jsx
@@ -3,14 +3,26 @@ import T1 from "../../Templates/T1/T1";
 import T2 from "../../Templates/T1/T2";
 import T3 from "../../Templates/T1/T3";
 import T4 from "../../Templates/T1/T4";
+import Carousel from "react-elastic-corousel";
+const breakPoints = [
+    {width:1, itemsToShow:1},
+    {width:550, itemsToShow:2},
+    {width:768, itemsToShow:3},
+    {width:1200, itemsToShow:4},
+];
+import React,{useState} from 'react';
 const BrowseTemplates = () => {
     return (
         <div className="all_templates">
-                <T1 /> 
-                <T2 />
-                <T3 />
-                <T4 />
-                      
+            <Carousel breakPoints={breakPoints}>
+                <Item><T1 /></Item>
+                <Item><T2 /></Item>
+                <Item><T3 /></Item>
+                
+                <ion-icon name="arrow-dropright-circle"></ion-icon>
+
+                <Item><T4 /></Item>
+            </Carousel>             
         </div>
     )
 }
